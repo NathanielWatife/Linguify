1337d5a4427b024e620faf936f555c1870106eed
NathanielWatife <awosanminathniel0@gmail.com>
2024-09-09 00:52:28 +0100

    Worked on urls and templates

--- transcriber/urls.py
+++ transcriber/urls.py
@@ -4,9 +4,8 @@ from . import views
 urlpatterns = [
     # home urls
     path('', views.homepage, name="homepage"),
-    # 
-    path('text-to-audio/', views.text_to_audio, name='text_to_audio'),
-    path('audio-to-text/', views.audio_to_text, name='audio_to_text'),
-    path('video-to-audio/', views.video_to_audio, name='video_to_audio'),
-    path('audio-to-video/', views.audio_to_video, name='audio_to_video'),
+    path('text-to-audio/', views.text_to_audio, name='text-to-audio'),
+    path('audio-to-text/', views.audio_to_text, name='audio-to-text'),
+    path('audio-to-video/', views.audio_to_video, name='audio-to-video'),
+    path('video-to-audio/', views.video_to_audio, name='video-to-audio'),
 ]
